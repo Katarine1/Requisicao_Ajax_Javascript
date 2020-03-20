acd0aa11c5f65ac2188accea3a6273ada8df531c
katarine1 <kba.2879@gmail.com>
2020-03-20 13:30:18 -0300

    Request Ajax

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+function requestPage(url) {
+
+	let ajax = new XMLHttpRequest();
+
+	ajax.open('GET', url);
+
+	console.log(ajax.responseText);
+
+	ajax.send();
+	
+	ajax.onreadystatechange = function() {
+		if(ajax.status == 200) {
+			console.log(ajax.status);
+			document.getElementById('header').style.display = 'none';
+			// Shows the text of the selected page.
+			document.getElementById('page').innerHTML = ajax.responseText;			
+		}
+
+		if(ajax.status == 404) {
+			console.log(ajax.status);
+			document.getElementById('page').innerHTML = 'Try again';			
+		}
+	}
+	
+	console.log(ajax);
+	
+}
